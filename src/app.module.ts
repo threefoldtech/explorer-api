fdede23fd197000a50f532ffc6af13b3ca2bd2f1
MohamedElmdary <engm5081@gmail.com>
2022-02-20 15:22:15 +0200

    Added Grid2 in caching layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { ExplorerService } from './explorer.service';
+import { Urls, IParams } from './types';
 
 @Module({
   imports: [
@@ -31,11 +32,35 @@ export class AppModule implements OnModuleInit {
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private readonly httpService: HttpService,
+    private readonly explorerService: ExplorerService,
   ) {}
 
+  private __fetchGrid2Data() {
+    for (const item of ['nodes', 'farms', 'gateways']) {
+      // Devnet
+      this.cacheManager.set(`grid2_devnet_${item}`, {
+        grid: 'grid2',
+        network: 'devnet',
+        url: `https://explorer.devnet.grid.tf/explorer/${item}`,
+        status: 'down',
+        data: [],
+      });
+
+      // Testnet
+      const testNodesUrl = IParams.getUrls({ grid: 'grid2', network: 'testnet' }, `/explorer/${item}`); // prettier-ignore
+      this.explorerService.fetchAll(testNodesUrl).subscribe(([data]) => {
+        this.cacheManager.set(`grid2_testnet_${item}`, data);
+      });
+
+      // mainnet
+      const mainNodesUrl = IParams.getUrls({ grid: 'grid2', network: 'mainnet' }, `/explorer/${item}`); // prettier-ignore
+      this.explorerService.fetchAll(mainNodesUrl).subscribe(([data]) => {
+        this.cacheManager.set(`grid2_mainnet_${item}`, data);
+      });
+    }
+  }
+
   onModuleInit() {
-    // this.httpService
-    //   .get('https://explorer.testnet.grid.tf/explorer/nodes')
-    //   .subscribe(({ data }) => console.log(data));
+    this.__fetchGrid2Data();
   }
 }
